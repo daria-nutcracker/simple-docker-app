ff47afb6aa9ba49af283bfe594c011fe704ca039
daria-nutcracker <dsshchelokova.42@gmail.com>
2023-10-15 23:18:15 +0300

    Initial commit

---
+++ simple-docker-app/src/main/java/ru/itgirl/simpledockerapp/Controllers/SimpleController.java
@@ -0,0 +1,12 @@
+package ru.itgirl.simpledockerapp.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Docker!";
+    }
+}
